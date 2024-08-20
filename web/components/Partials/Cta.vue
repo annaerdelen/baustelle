c80a31a909d8c74103f8c625882040c7b7f7825e
Anna Erdelen <services@erdelen.com>
2024-08-20 15:51:28 +0200

    refactor: nuxtlink cta

--- web/components/Partials/Cta.vue
+++ web/components/Partials/Cta.vue
@@ -1,18 +1,16 @@
 <template>
-  <div v-if="cta && (cta.slug || cta.page || cta.href)">
-    <NuxtLink v-if="cta.type === 'internalLink'" :class="[classes]" :to="cta.slug || cta.page">
-      {{ cta.title }}
-    </NuxtLink>
-    <a v-if="cta.type === 'externalLink'" :class="[classes]" :href="cta.href" target="_blank" rel="noopener noreferrer">
-      {{ cta.title }}
-    </a>
-  </div>
+  <NuxtLink
+    v-if="cta && (cta.slug || cta.page || cta.href)"
+    class="bg-black text-white"
+    :to="cta.type === 'internalLink' ? cta.slug || cta.page : cta.href"
+    :target="cta.type === 'externalLink' ? '_blank' : ''"
+  >
+    {{ cta.title }}
+  </NuxtLink>
 </template>
 
 <script setup>
 defineProps({
   cta: Object,
 });
-
-const classes = 'bg-black text-white';
 </script>
